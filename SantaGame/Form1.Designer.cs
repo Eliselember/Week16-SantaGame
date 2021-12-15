d16f113cfa60bd091ed3c47cfd651e9741f4a367
Opilane TTHK <opilane@tthk.ee>
2021-12-15 10:40:55 +0200

    week16

---
+++ SantaGame/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace SantaGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.päike = new System.Windows.Forms.PictureBox();
+            this.ground = new System.Windows.Forms.PictureBox();
+            this.santa = new System.Windows.Forms.PictureBox();
+            this.snowflake = new System.Windows.Forms.PictureBox();
+            this.house = new System.Windows.Forms.PictureBox();
+            this.tree = new System.Windows.Forms.PictureBox();
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.playAgain = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.päike)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.santa)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.snowflake)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.house)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tree)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // päike
+            // 
+            this.päike.Image = ((System.Drawing.Image)(resources.GetObject("päike.Image")));
+            this.päike.Location = new System.Drawing.Point(343, 26);
+            this.päike.Name = "päike";
+            this.päike.Size = new System.Drawing.Size(124, 78);
+            this.päike.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.päike.TabIndex = 1;
+            this.päike.TabStop = false;
+            // 
+            // ground
+            // 
+            this.ground.Image = ((System.Drawing.Image)(resources.GetObject("ground.Image")));
+            this.ground.Location = new System.Drawing.Point(-4, 398);
+            this.ground.Name = "ground";
+            this.ground.Size = new System.Drawing.Size(483, 135);
+            this.ground.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ground.TabIndex = 2;
+            this.ground.TabStop = false;
+            // 
+            // santa
+            // 
+            this.santa.Image = ((System.Drawing.Image)(resources.GetObject("santa.Image")));
+            this.santa.Location = new System.Drawing.Point(12, 110);
+            this.santa.Name = "santa";
+            this.santa.Size = new System.Drawing.Size(128, 107);
+            this.santa.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.santa.TabIndex = 3;
+            this.santa.TabStop = false;
+            // 
+            // snowflake
+            // 
+            this.snowflake.Image = ((System.Drawing.Image)(resources.GetObject("snowflake.Image")));
+            this.snowflake.Location = new System.Drawing.Point(186, 12);
+            this.snowflake.Name = "snowflake";
+            this.snowflake.Size = new System.Drawing.Size(127, 74);
+            this.snowflake.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.snowflake.TabIndex = 4;
+            this.snowflake.TabStop = false;
+            // 
+            // house
+            // 
+            this.house.Image = ((System.Drawing.Image)(resources.GetObject("house.Image")));
+            this.house.Location = new System.Drawing.Point(331, 329);
+            this.house.Name = "house";
+            this.house.Size = new System.Drawing.Size(136, 115);
+            this.house.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.house.TabIndex = 5;
+            this.house.TabStop = false;
+            // 
+            // tree
+            // 
+            this.tree.Image = ((System.Drawing.Image)(resources.GetObject("tree.Image")));
+            this.tree.Location = new System.Drawing.Point(12, 299);
+            this.tree.Name = "tree";
+            this.tree.Size = new System.Drawing.Size(157, 145);
+            this.tree.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.tree.TabIndex = 6;
+            this.tree.TabStop = false;
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.Font = new System.Drawing.Font("Arial", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.scoreLabel.Location = new System.Drawing.Point(12, 26);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(126, 34);
+            this.scoreLabel.TabIndex = 7;
+            this.scoreLabel.Text = "score: 0";
+            this.scoreLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // playAgain
+            // 
+            this.playAgain.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.playAgain.Font = new System.Drawing.Font("Arial Narrow", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.playAgain.Location = new System.Drawing.Point(201, 189);
+            this.playAgain.Name = "playAgain";
+            this.playAgain.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.playAgain.Size = new System.Drawing.Size(168, 110);
+            this.playAgain.TabIndex = 8;
+            this.playAgain.Text = "PLAY AGAIN";
+            this.playAgain.UseVisualStyleBackColor = true;
+            this.playAgain.Visible = false;
+            this.playAgain.Click += new System.EventHandler(this.playAgain_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.ClientSize = new System.Drawing.Size(479, 516);
+            this.Controls.Add(this.playAgain);
+            this.Controls.Add(this.scoreLabel);
+            this.Controls.Add(this.tree);
+            this.Controls.Add(this.house);
+            this.Controls.Add(this.snowflake);
+            this.Controls.Add(this.santa);
+            this.Controls.Add(this.ground);
+            this.Controls.Add(this.päike);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.päike)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.santa)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.snowflake)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.house)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tree)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox päike;
+        private System.Windows.Forms.PictureBox ground;
+        private System.Windows.Forms.PictureBox santa;
+        private System.Windows.Forms.PictureBox snowflake;
+        private System.Windows.Forms.PictureBox house;
+        private System.Windows.Forms.PictureBox tree;
+        private System.Windows.Forms.Label scoreLabel;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button playAgain;
+    }
+}
+
---
+++ SantaGame/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SantaGame
+{
+    public partial class Form1 : Form
+    {
+        int gravity = 10;
+        int santaSpeed = 6;
+        int score = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            santa.Top += gravity;
+            päike.Left -= santaSpeed;
+            house.Left -= santaSpeed;
+            scoreLabel.Text = $"score: {score}";
+
+            if(päike.Left < -100)
+            {
+                päike.Left = 500;
+                score++;
+            }
+            if(house.Left < -100)
+            {
+                house.Left = 600;
+                score++;
+            }
+
+            if (santa.Bounds.IntersectsWith(house.Bounds) || santa.Bounds.IntersectsWith(päike.Bounds) ||
+                santa.Bounds.IntersectsWith(ground.Bounds))
+            {
+                gameOver();
+            }
+
+            if(santa.Top < -25)
+            {
+                gameOver();
+            }
+            
+            
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                gravity = -5;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                gravity = 5;
+            }
+        }
+
+        private void gameOver()
+        {
+            timer1.Stop();
+            scoreLabel.Text = $"Game Over!";
+            playAgain.Visible = true;
+        }
+
+        private void playAgain_Click(object sender, EventArgs e)
+        {
+            Form1 NewFrom = new Form1();
+            NewFrom.Show();
+            this.Dispose(false);
+        }
+    }
+}
